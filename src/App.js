c043a0e9b322f5a550478a2a7ebabf1fd864f75f
Pritam Majumder <ptmmajumder630@gmail.com>
2021-09-02 21:26:29 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import Nav from './Nav';
+import Nav from './navbar';
 
 
 
--- src/navbar.js
+++ src/navbar.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import "./nav.css";
+import "./navbar.css";
 
 function Nav() {
 
