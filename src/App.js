a0b6d5ff9b3016a3c7e150dd7c5eed688e385cb3
Pritam Majumder <ptmmajumder630@gmail.com>
2021-09-02 21:09:03 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import './App.css';
-import HomeScreen from './HomeScreen';
+
 import Nav from './Nav';
-import Footer from './footer';
-import Banner from './banner';
+
 
 
 function App() {
   return (
     <div className="app">
-      <HomeScreen />
       <Nav />
-      <Footer />
-      <Banner />
     </div>
   );
 }
