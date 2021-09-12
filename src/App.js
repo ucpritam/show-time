ca4c57bcad2b1ad5564d33b73f8e982c709cba48
Akash0601 <akash111shaw@gmail.com>
2021-09-12 15:04:21 +0530

    firebase added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Loginscreen from './screens/loginscreen';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomeScreen from './screens/homescreen';
+import { auth } from "./firebase"; 
+import { useDispatch, useSelector } from "react-redux";
+import { logout, login, selectUser } from "./features/userSlice";
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+      const unsubscribe = auth.onAuthStateChanged(userAuth => {
+        if (userAuth) { 
+           dispatch(login({
+             uid: userAuth.uid,
+             email: userAuth.email,
+           }))
+        }else{
+          //logged out
+          dispatch(logout);
+        }
+      } );
+
+      return unsubscribe;
+  }, []);
+
   return (
     <div className="app">
-    {/* <Loginscreen /> */}
-    <HomeScreen />
+    <Loginscreen />
+    {/* <HomeScreen /> */}
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,18 @@
+import firebase from 'firebase';
+
+
 const firebaseConfig = {
-    apiKey: "AIzaSyAo-bMCQoEJl3rxIOqEx8hSdHFJ4HCynJ8",
-    authDomain: "netflix-clone-84910.firebaseapp.com",
-    projectId: "netflix-clone-84910",
-    storageBucket: "netflix-clone-84910.appspot.com",
-    messagingSenderId: "750893698981",
-    appId: "1:750893698981:web:cf8089e3a9129e8a73224c"
-  }; 
+  apiKey: "AIzaSyBP5s8d9gr2mHomYt5_rQNdmNSs414Tnjk",
+  authDomain: "showtime-5720a.firebaseapp.com",
+  projectId: "showtime-5720a",
+  storageBucket: "showtime-5720a.appspot.com",
+  messagingSenderId: "112230973607",
+  appId: "1:112230973607:web:acfac73a8c048e767c9b82"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export { auth };
+  export default db;
